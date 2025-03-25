330d571c60c7ec85f6b4b9333c175031fbc965fc
CassiaSantos <cassia.oliveira.profissional@gmail.com>
2025-03-24 21:13:07 -0300

    feat: salva no localStorage as transações cadastradas no formulário

--- public/js/home.js
+++ public/js/home.js
@@ -26,9 +26,35 @@ function checkLogged() {
     if(dataUser) {
         data = JSON.parse(dataUser);
     }
-
-    console.log(data);
 }
+
+//Adiciona um lancamento na tabela:
+document.getElementById("transaction-form").addEventListener("submit", function(e) {
+    e.preventDefault(); //previnir comportamento padrão do form de redirecionar para outra página;
+
+    const descriptionTransaction = document.getElementById("description-transaction-input").value;
+    const valueTransaction = parseFloat(document.getElementById("value-transaction-input").value); //pega o valor do input e transforma em float para que seja possível fazer somas;
+    const dateTransaction = document.getElementById("date-transaction-input").value;
+    const typeTransaction = document.querySelector('input[name="type-input"]:checked').value; //pega o valor de um input que está com nome "type-input" que estiver marcado com o checked;
+
+    data.transactions.unshift({ //uso de unshift para adicionar um item no inicio da lista; push adiciona no fim da lista;
+        description: descriptionTransaction,
+        value: valueTransaction,
+        date: dateTransaction,
+        type: typeTransaction
+    });
+
+    saveTransaction(data);
+    alert("Lançamento adicionado com sucesso!");
+    e.target.reset(); //limpa o formulário;
+    myModal.hide(); //fecha o modal;
+})
+
+
+// Função para salvar uma transação no localstorage:
+function saveTransaction(data) {
+    localStorage.setItem(data.email, JSON.stringify(data));
+}   
 
 document.getElementById("button-logout").addEventListener("click", logout);
 
